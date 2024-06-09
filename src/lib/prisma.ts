5178fd43cd297d2aa5f608910341a705f362b44e
Tiaansu <marlonrivera12321@gmail.com>
2024-06-09 11:44:29 +0800

    chore: removing serverless libraries.
    It is so dumb of mine to use postgres libraries on mongodb

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 
 const prismaClientSingleton = () => {
-    const neon = new Pool({ connectionString: process.env.DATABASE_URL });
-    const adapter = new PrismaNeon(neon);
-    return new PrismaClient({ adapter });
+    return new PrismaClient();
 };
 
 declare global {
